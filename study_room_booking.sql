33fa9acd8bf1c2a4b98640b63b3b107f2c70a3be
lexi1025 <2737536754@qq.com>
2026-06-29 15:01:19 +0800

    change

--- study_room_booking.sql
+++ study_room_booking.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 50744
  Source Host           : localhost:3306
- Source Schema         : python_room
+ Source Schema         : study_room_booking
 
  Target Server Type    : MySQL
  Target Server Version : 50744
